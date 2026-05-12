f163b4d83d4e40e36c204e88a300353ad6006732
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-05-12 16:33:25 +0100

    chore: add environment config and wire up TypeORM with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,33 @@ import { UsersModule } from './users/users.module';
 import { PollsModule } from './polls/polls.module';
 import { VotesModule } from './votes/votes.module';
 import { ResultsModule } from './results/results.module';
+import { ConfigModule, ConfigService, getConfigToken } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UsersModule, PollsModule, VotesModule, ResultsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+    PollsModule,
+    VotesModule,
+    ResultsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
