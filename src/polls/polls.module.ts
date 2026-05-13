81ad0340b10babdc98de2292e8a5500b825cefc3
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-05-13 15:48:00 +0100

    feat: implement PollsService with CRUD and status management

--- src/polls/polls.module.ts
+++ src/polls/polls.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Poll } from './entities/poll.entity';
+import { PollOption } from './entities/poll-option.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Poll, PollOption])],
   controllers: [PollsController],
-  providers: [PollsService]
+  providers: [PollsService],
+  exports: [PollsService],
 })
 export class PollsModule {}
--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Poll, PollStatus } from './entities/poll.entity';
+import { Repository } from 'typeorm';
+import { PollOption } from './entities/poll-option.entity';
+import { CreatePollDto } from './dto/create-poll.dto';
+import { User } from 'src/users/entities/user.entity';
+import { UpdatePollDto } from './dto/update-poll.dto';
 
 @Injectable()
-export class PollsService {}
+export class PollsService {
+  constructor(
+    @InjectRepository(Poll)
+    private readonly pollRepository: Repository<Poll>,
+
+    @InjectRepository(PollOption)
+    private readonly pollOptionRepository: Repository<PollOption>,
+  ) {}
+
+  async create(dto: CreatePollDto, creator: User): Promise<Poll> {
+    const poll = this.pollRepository.create({
+      title: dto.title,
+      description: dto.description,
+      createdBy: creator,
+    });
+
+    const savedPoll = await this.pollRepository.save(poll);
+
+    const options = dto.options.map((optionText) =>
+      this.pollOptionRepository.create({ optionText, poll: savedPoll }),
+    );
+    await this.pollOptionRepository.save(options);
+
+    return this.findOne(savedPoll.id);
+  }
+
+  async findAll(status?: PollStatus): Promise<Poll[]> {
+    const query = this.pollRepository
+      .createQueryBuilder('poll')
+      .leftJoinAndSelect('poll.options', 'options')
+      .leftJoinAndSelect('poll.createdBy', 'createdBy')
+      .orderBy('poll.createdAt', 'DESC');
+
+    if (status) {
+      query.where('poll.status = :status', { status });
+    }
+
+    return query.getMany();
+  }
+
+  async findOne(id: string): Promise<Poll> {
+    const poll = await this.pollRepository.findOne({
+      where: { id },
+      relations: ['options', 'createdBy'],
+    });
+
+    if (!poll) throw new NotFoundException(`Poll not found`);
+    return poll;
+  }
+
+  async update(id: string, dto: UpdatePollDto): Promise<Poll> {
+    const poll = await this.findOne(id);
+    Object.assign(poll, dto);
+    await this.pollRepository.save(poll);
+    return this.findOne(id);
+  }
+
+  async updateStatus(id: string, status: PollStatus): Promise<Poll> {
+    const poll = await this.findOne(id);
+    poll.status = status;
+    await this.pollRepository.save(poll);
+    return this.findOne(id);
+  }
+
+  async remove(id: string): Promise<void> {
+    const poll = await this.findOne(id);
+    await this.pollRepository.remove(poll);
+  }
+}
