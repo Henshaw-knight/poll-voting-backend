57b454f395260949160b46b003b11af0b4894514
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-05-13 11:36:03 +0100

    feat: add signup and login DTOs with validation

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  state: string;
+}
