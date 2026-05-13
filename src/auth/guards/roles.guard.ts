848802351ed1041527366f94a0efc89ba5683307
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-05-13 12:43:29 +0100

    feat: add JWT strategy and auth guards

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+    const hasRole = requiredRoles.includes(user.role);
+
+    if (!hasRole) throw new ForbiddenException('Access denied');
+    return true;
+  }
+}
