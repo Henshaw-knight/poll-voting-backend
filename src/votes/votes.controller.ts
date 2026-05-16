e92ced2d86264295fc5e8e2fda23b2c1d3152286
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-05-16 08:50:07 +0100

    feat: implement VotesController for vote submission

--- src/votes/votes.controller.ts
+++ src/votes/votes.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Request } from '@nestjs/common';
+import { VotesService } from './votes.service';
+import { CreateVoteDto } from './dto/create-vote.dto';
 
 @Controller('votes')
-export class VotesController {}
+export class VotesController {
+  constructor(private readonly votesService: VotesService) {}
+
+  @Post()
+  castVote(@Body() dto: CreateVoteDto, @Request() req: any) {
+    return this.votesService.castVote(dto, req.user);
+  }
+}
