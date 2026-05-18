1b9d493b9db6dfda2a267d2a1ba092de1e91f7c1
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-05-18 14:31:22 +0100

    Complete work on results module

--- src/results/results.module.ts
+++ src/results/results.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ResultsController } from './results.controller';
 import { ResultsService } from './results.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vote } from 'src/votes/entities/vote.entity';
+import { PollsModule } from 'src/polls/polls.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vote]), PollsModule],
   controllers: [ResultsController],
-  providers: [ResultsService]
+  providers: [ResultsService],
 })
 export class ResultsModule {}
