e6355a869a32c00709b1593bce46e0b4ea41d5e8
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-05-18 14:28:58 +0100

    feat: implement ResultsController for results retrieval

--- src/results/results.controller.ts
+++ src/results/results.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ResultsService } from './results.service';
 
 @Controller('results')
-export class ResultsController {}
+@UseGuards(JwtAuthGuard)
+export class ResultsController {
+  constructor(private readonly resultsService: ResultsService) {}
+
+  @Get(':pollId')
+  getResults(@Param('pollId') pollId: string, @Query('state') state?: string) {
+    return this.resultsService.getResults(pollId, state);
+  }
+}
